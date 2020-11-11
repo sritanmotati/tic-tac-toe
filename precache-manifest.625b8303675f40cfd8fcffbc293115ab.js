8c42747376bb915a7236b904b582628d6289ae8e
Sritan Motati <sritanmotati@gmail.com>
2020-11-10 21:41:26 -0500

    Updates

---
+++ precache-manifest.625b8303675f40cfd8fcffbc293115ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fd0d5603c2a985a643964eee387593",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "66d889a6eeadcadb6f7d",
+    "url": "/tic-tac-toe/static/css/main.a5d0af84.chunk.css"
+  },
+  {
+    "revision": "a31af468cabe68785753",
+    "url": "/tic-tac-toe/static/js/2.1fc8b45f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.1fc8b45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d889a6eeadcadb6f7d",
+    "url": "/tic-tac-toe/static/js/main.b9adda62.chunk.js"
+  },
+  {
+    "revision": "61109fc60463736a7ada",
+    "url": "/tic-tac-toe/static/js/runtime-main.12138588.js"
+  }
+]);
